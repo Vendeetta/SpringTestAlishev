209b0c1a3149d6fe1e00acc2b74efc7a0c2ceb4c
Vendeetta <7865330@gmail.com>
2023-01-05 14:16:57 +0300

    testCom

--- SpringTestAlishev/src/main/TestSpring.java
+++ SpringTestAlishev/src/main/TestSpring.java
@@ -7,9 +7,11 @@ public class TestSpring {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
-        //testCom
+
     }
 }
--- SpringTestAlishev/src/main/music/ClassicalMusic.java
+++ SpringTestAlishev/src/main/music/ClassicalMusic.java
@@ -2,11 +2,18 @@ package music;
 
 public class ClassicalMusic implements Music{
 
-    public ClassicalMusic() {
+    private ClassicalMusic() {
     }
 
     @Override
     public String getSong() {
         return "Playing Mozart";
+    }
+
+    public void init(){
+        System.out.println("init");
+    }
+    public void destr(){
+        System.out.println("destr");
     }
 }
